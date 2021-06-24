9d33da87ae9b3320adeb0a012268e437f4ec98ce
Roshni1998 <roshnimali847@gmail.com>
2021-06-24 10:30:17 +0530

    Added: Address Book System UC-1

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String []args){
         System.out.println("Welcome to Address Book System");
+        AddressBook addressBook = new AddressBook(5);
+        addressBook.addContact("Rohit", "Sharma", "Malviya Nagar", "Udaipur", "Rajasthan", "34267","7345682934", "rohit123@");
+        addressBook.addContact("Rajeev", "Malhotra", "Shastri Circle", "Ahmedabad", "Gujarat", "56743", "9998889994", "rajeev@");
+        System.out.println(addressBook);
     }
 }
